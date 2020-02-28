20151f4b6cf5b5461af5a8b9400a32cfe4d6063d
Siri Khalsa <skhalsa10@live.com>
2020-02-27 22:00:39 -0700

    added some animation

--- Project3/1d_density.py
+++ Project3/1d_density.py
@@ -14,15 +14,20 @@ def setup():
     size(480, 120)
 
 def draw():
+    background(50)
+    global x
     if  mouse_is_pressed:
         fill(0)
     else:
         fill(255)
-    ellipse((50,50),20,20,mode=RADIUS)
+    ellipse((x,50),20,20,mode=RADIUS)
+    x += 1
+    if x>width:
+        x=0
+    print(x)
+
     
 
-def key_pressed():
-    background(24)
 
 
 
