4f9cf83340d0d3be67dfd971f7dc603828f274f9
Siri Khalsa <skhalsa10@live.com>
2020-03-30 19:06:09 -0600

    working onthe code for part 1d

---
+++ Project2/Code/Part1/Part1d.py
@@ -0,0 +1,40 @@
+from spike import *
+from neutral_network import *
+import random
+
+
+b = 10
+total_dead = 0
+average_mutations_dead = 0
+average_mutations_completed = 0
+total_completed = 0
+max_hist = 0
+non_neutral_indices = []
+total_mutations = 0
+
+population = [Spike() for x in range(100)]
+
+while total_completed == 0:
+    for i in range(100):
+        population[i].mutate()
+        if(not isGenomeNeutral(population[i].getAminoAcids())):
+            non_neutral_indices.append(i)
+        hist = len(population[i].history)
+        max_hist = max(max_hist,hist)
+        if(isSARS(population[i].getAminoAcids())):
+            total_completed += 1
+
+    to_remove = len(non_neutral_indices)-b
+    while(to_remove>0):
+        # pick a random index to kill a nonneutral
+        index_To_Pop = random.randint(0,to_remove+(b-1))
+        index = non_neutral_indices.pop(index_To_Pop)
+        # now set population at this index to a new spike
+        population[index] = Spike()
+        total_dead += 1
+        to_remove -= 1
+
+    print(total_mutations)
+    total_mutations += 1
+
+print("total mutations needed to get sars " + str(total_mutations))
---
+++ Project2/Code/Part1/neutral_network.py
@@ -0,0 +1,18 @@
+"""
+This module class thing can be used as a utility class to confirm that a genome falls
+into the defined  neutral network in this file
+"""
+SARS_2002 = 'YLNYT'
+pos0 = ['L','S','Y']
+pos1 = ['F', 'L']
+pos2 = ['Q','N','K']
+pos3 = ['Q', 'Y']
+pos4 = ['N','S','T']
+neutral_genomes = [a+b+c+d+e for a in pos0 for b in pos1 for c in pos2 for d in pos3 for e in pos4]
+
+
+def isGenomeNeutral(genome):
+    return neutral_genomes.__contains__(genome)
+
+def isSARS(genome):
+    return genome == SARS_2002
