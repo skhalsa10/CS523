f4e8cc86c6303cbceb28a01562c4c9f3695869c0
Anas Gauba <anas.farooq71@gmail.com>
2020-05-02 14:22:39 -0600

    Initial BuildingManager setup

--- Project3/AgentBasedEpidemicModel/src/abm/buildingmanager/BuildingManager.java
+++ Project3/AgentBasedEpidemicModel/src/abm/buildingmanager/BuildingManager.java
@@ -5,18 +5,34 @@ import abm.utils.Communicator;
 import abm.utils.messages.Message;
 import abm.utils.messages.Shutdown;
 
+import java.util.HashMap;
 import java.util.concurrent.PriorityBlockingQueue;
 
+/**
+ * @version 1.0.0
+ * @author Anas Gauba
+ */
 public class BuildingManager extends Thread implements Communicator {
 
     private PriorityBlockingQueue<Message> messages;
     private ABMController abmController;
     private boolean isRunning;
+    private HashMap<Integer, Community> communities;
+    private HashMap<Integer, Hotel> hotels;
+    private HashMap<Integer, Resturant> restaurants;
+    private HashMap<Integer, GroceryStore> groceryStores;
+    private HashMap<Integer, Hospital> hospitals;
 
     public BuildingManager(ABMController abmController) {
         this.abmController = abmController;
         this.messages = new PriorityBlockingQueue<>();
         //TODO: Instantiate child classes here, other relevant stuff.
+        this.communities = new HashMap<>();
+        this.hotels = new HashMap<>();
+        this.restaurants = new HashMap<>();
+        this.groceryStores = new HashMap<>();
+        this.hospitals = new HashMap<>();
+
         this.isRunning = true;
         start();
     }
